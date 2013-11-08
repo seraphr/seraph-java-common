f79db031e9a619dd90e261bc96abfa4112aadbf3
seraph <seraph@Noctowl2>
2013-11-09 00:46:22 +0900

    IdFunctionにGenericInstanceを追加

--- src/main/java/jp/seraphr/common/IdFunction.java
+++ src/main/java/jp/seraphr/common/IdFunction.java
@@ -7,6 +7,13 @@ package jp.seraphr.common;
  * 引数をそのまま返すFunction実装
  */
 public class IdFunction<_T> implements Function<_T, _T> {
+    private static final IdFunction<?> GENERIC_INSTANCE = new IdFunction<Object>();
+
+    @SuppressWarnings("unchecked")
+    public static <E> IdFunction<E> getInstance() {
+        return (IdFunction<E>) GENERIC_INSTANCE;
+    }
+
     @Override
     public _T convert(_T aSource) {
         return aSource;
